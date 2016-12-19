61be3520177f19654e270ff42095597dea161e36
Teresa Meyer <tmeyer2@nd.edu>
2016-12-19 04:32:22 +0000

    added nd workflow helper with attributes definitions

---
+++ app/helpers/nd_application_workflow/nd_workflows_helper.rb
@@ -0,0 +1,31 @@
+module NdApplicationWorkflow
+  module NdWorkflowHelper
+
+    def nd_workflows_attributes
+      [ :id, :workflow_type, :auto_or_manual, :workflow_custom_type, :assigned_to_netid,
+        :assigned_to_first_name, :assigned_to_last_name, :email_notes, :email_include_detail,
+        :approval_notes, :approved_date, :approved_by_netid, :created_by_netid, :workflow_state,
+        :nd_workflow_details_attributes => nd_workflow_details_attributes ]
+    end
+
+    def nd_workflow_details_attributes
+      [
+        :id,
+        :employee_id,
+        :pidm,
+        :posn,
+        :suffix,
+        :employee_class,
+        :primary,
+        :start_date,
+        :end_date,
+        :title,
+        :status,
+        :status_description,
+        :last_paid,
+        :orgn_code,
+        :orgn_title,
+        :pict_code ]
+    end
+  end
+end
